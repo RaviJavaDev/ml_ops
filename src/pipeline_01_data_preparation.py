3589015396d3f8a2e1895a6e0b30dd7e2cc143ee
ravis <ravisonawane20@gmai.com>
2021-03-17 11:50:33 +0530

    '-files'

--- src/pipeline_01_data_preparation.py
+++ src/pipeline_01_data_preparation.py
@@ -11,15 +11,15 @@ def read_params(config_path):
 
 
 def main(config_path, datasource):
-    config = read_params(config_path=config_path)
+    config = read_params(config_path)
     print(config)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = argparse.ArgumentParser()
-    default_config_path = os.path.join('config', 'params.yaml')
-    print('path',default_config_path)
-    args.add_argument('--config', default=default_config_path)
-    args.add_argument('--datasource', default=None)
+    default_config_path = os.path.join("./../config", 'params.yaml')
+    args.add_argument("--config", default=default_config_path)
+    args.add_argument("--datasource", default=None)
+
     parsed_args = args.parse_args()
     main(config_path=parsed_args.config, datasource=parsed_args.datasource)
